f496d913b866946a37f89a115fb0d0a886b24d59
Guilherme Marques Lopes <105084876+GuilhLopes@users.noreply.github.com>
2024-11-21 22:52:28 -0300

    Create navBar.vue

---
+++ src/components/common/navBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+      <router-link to="/cadastro">Cadastro</router-link>
+      <router-link to="/listagem">Listagem</router-link>
+      <router-link to="/delecao">Deleção</router-link>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: "NavBar",
+  };
+  </script>
+  
+  <style scoped>
+  nav {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 20px;
+  }
+  </style>
+  
